f2b98537a543e03293b1fe51738323e5d334ea6c
Nicolas Froidure <nicolas.froidure@gmail.com>
2019-02-10 16:08:14 +0100

    feat(whook-swagger-ui): Add a `getOpenAPI` endpoint

---
+++ packages/create-whook/src/handlers/getOpenAPI.js
@@ -0,0 +1,8 @@
+import getOpenAPI, {
+  definition,
+} from 'whook-swagger-ui/dist/handlers/getOpenAPI';
+
+// TODO: Use WHOOK_PLUGINS to get handlers from plugins
+// instead of proxying here
+export { definition };
+export default getOpenAPI;
---
+++ packages/whook-swagger-ui/src/handlers/getOpenAPI.test.js
@@ -0,0 +1,61 @@
+import initGetOpenAPI from './getOpenAPI';
+
+describe('getOpenAPI', () => {
+  const API = {
+    swagger: '2.0',
+    paths: {
+      '/time': {
+        get: {
+          tags: ['public'],
+          'x-whook': { memx: 2, tx: 18 },
+        },
+        put: {
+          tags: [],
+          'x-whook': { private: true },
+        },
+      },
+    },
+  };
+
+  it('should work', async () => {
+    const getOpenAPI = await initGetOpenAPI({
+      API,
+    });
+    const response = await getOpenAPI({});
+
+    expect({
+      response: {
+        ...response,
+        body: {
+          ...response.body,
+          info: {
+            ...response.body.info,
+            version: '<already_tested>',
+          },
+        },
+      },
+    }).toMatchSnapshot();
+  });
+
+  it('should show every endpoints when authenticated', async () => {
+    const getOpenAPI = await initGetOpenAPI({
+      API,
+    });
+    const response = await getOpenAPI({
+      userId: 1,
+    });
+
+    expect({
+      response: {
+        ...response,
+        body: {
+          ...response.body,
+          info: {
+            ...response.body.info,
+            version: '<already_tested>',
+          },
+        },
+      },
+    }).toMatchSnapshot();
+  });
+});
