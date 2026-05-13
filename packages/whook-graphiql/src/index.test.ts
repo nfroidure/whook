21ebf71e230bf566c6e2f4447204dc92416a1fd6
Nicolas Froidure <nicolas.froidure@gmail.com>
2026-05-13 15:38:59 +0200

    refactor(@whook/graphiql): improve graphiql dev tool

--- packages/whook-graphiql/src/index.test.ts
+++ packages/whook-graphiql/src/index.test.ts
@@ -417,137 +417,106 @@ describe('wrapHTTPRouterWithGraphIQL', () => {
            "HTTP_SERVER_OPTIONS",
          ],
        ],
-       "data": "
-     <!DOCTYPE html>
-     <html>
-     <head>
-       <meta charset="utf-8" />
-       <title>GraphiQL</title>
-       <meta name="robots" content="noindex" />
-       <style>
-         html, body {
-           height: 100%;
-           margin: 0;
-           overflow: hidden;
-           width: 100%;
-         }
-       </style>
-       <link href="//unpkg.com/graphiql@0.11.11/graphiql.css" rel="stylesheet" />
-       <script src="//unpkg.com/react@15.6.1/dist/react.min.js"></script>
-       <script src="//unpkg.com/react-dom@15.6.1/dist/react-dom.min.js"></script>
-       <script src="//unpkg.com/graphiql@0.11.11/graphiql.min.js"></script>
-       
-       <script src="//cdn.jsdelivr.net/fetch/2.0.1/fetch.min.js"></script>
-       
-       
-
-     </head>
-     <body>
-       <script>
-         // Collect the URL parameters
-         var parameters = {};
-         window.location.search.substr(1).split('&').forEach(function (entry) {
-           var eq = entry.indexOf('=');
-           if (eq >= 0) {
-             parameters[decodeURIComponent(entry.slice(0, eq))] =
-               decodeURIComponent(entry.slice(eq + 1));
+       "data": "<!doctype html>
+     <html lang="en">
+       <head>
+         <meta charset="UTF-8" />
+         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+         <title>Whook GraphiQL Explorer</title>
+         <style>
+           body {
+             margin: 0;
            }
-         });
-         // Produce a Location query string from a parameter object.
-         function locationQuery(params, location) {
-           return (location ? location: '') + '?' + Object.keys(params).map(function (key) {
-             return encodeURIComponent(key) + '=' +
-               encodeURIComponent(params[key]);
-           }).join('&');
-         }
-         // Derive a fetch URL from the current URL, sans the GraphQL parameters.
-         var graphqlParamNames = {
-           query: true,
-           variables: true,
-           operationName: true
-         };
-         var otherParams = {};
-         for (var k in parameters) {
-           if (parameters.hasOwnProperty(k) && graphqlParamNames[k] !== true) {
-             otherParams[k] = parameters[k];
+
+           #graphiql {
+             height: 100dvh;
            }
-         }
 
-         
-
-         
-           // We don't use safe-serialize for location, because it's not client input.
-           var fetchURL = locationQuery(otherParams, '/v1/graphql');
-
-           // Defines a GraphQL fetcher using the fetch API.
-           function graphQLHttpFetcher(graphQLParams) {
-               return fetch(fetchURL, {
-                 method: 'post',
-                 headers: {
-                   'Accept': 'application/json',
-                   'Content-Type': 'application/json',
-                   'Authorization': 'Bearer oudelali'
-                 },
-                 body: JSON.stringify(graphQLParams),
-                 credentials: 'same-origin',
-               }).then(function (response) {
-                 return response.text();
-               }).then(function (responseBody) {
-                 try {
-                   return JSON.parse(responseBody);
-                 } catch (error) {
-                   return responseBody;
-                 }
-               });
+           .loading {
+             height: 100%;
+             display: flex;
+             align-items: center;
+             justify-content: center;
+             font-size: 4rem;
            }
-         
+         </style>
+         <link
+           rel="stylesheet"
+           href="https://esm.sh/graphiql@5.2.2/dist/style.css"
+           integrity="sha384-f6GHLfCwoa4MFYUMd3rieGOsIVAte/evKbJhMigNdzUf52U9bV2JQBMQLke0ua+2"
+           crossorigin="anonymous"
+         />
+         <link
+           rel="stylesheet"
+           href="https://esm.sh/@graphiql/plugin-explorer@5.1.1/dist/style.css"
+           integrity="sha384-vTFGj0krVqwFXLB7kq/VHR0/j2+cCT/B63rge2mULaqnib2OX7DVLUVksTlqvMab"
+           crossorigin="anonymous"
+         />
+         <!--
+          * Note:
+          * The ?standalone flag bundles the module along with all of its \`dependencies\`, excluding \`peerDependencies\`, into a single JavaScript file.
+          * \`@emotion/is-prop-valid\` is a shim to remove the console error \` module "@emotion /is-prop-valid" not found\`. Upstream issue: https://github.com/motiondivision/motion/issues/3126
+         -->
+         <script type="importmap">
+           {
+             "imports": {
+               "react": "https://esm.sh/react@19.2.5",
+               "react/": "https://esm.sh/react@19.2.5/",
+               "react-dom": "https://esm.sh/react-dom@19.2.5",
+               "react-dom/": "https://esm.sh/react-dom@19.2.5/",
+               "graphiql": "https://esm.sh/graphiql@5.2.2?standalone&external=react,react-dom,@graphiql/react,graphql",
+               "graphiql/": "https://esm.sh/graphiql@5.2.2/",
+               "@graphiql/plugin-explorer": "https://esm.sh/@graphiql/plugin-explorer@5.1.1?standalone&external=react,@graphiql/react,graphql",
+               "@graphiql/react": "https://esm.sh/@graphiql/react@0.37.3?standalone&external=react,react-dom,graphql,@graphiql/toolkit,@emotion/is-prop-valid",
+               "@graphiql/toolkit": "https://esm.sh/@graphiql/toolkit@0.11.3?standalone&external=graphql",
+               "graphql": "https://esm.sh/graphql@16.13.2",
+               "@emotion/is-prop-valid": "data:text/javascript,"
+             },
+             "integrity": {
+               "https://esm.sh/react@19.2.5": "sha384-ZNmUQ9QQgyl95nnD/FJTBQn2ZEPTbWtMuWCXTKWNuF6Si7nC+6bvSgk5LWu+ELHn",
+               "https://esm.sh/react-dom@19.2.5": "sha384-qtNxBzn9gBs3CmJItMuvIVyjW3VIU0/rzGhCm9MippVU1BpR/c4VgaFYDIg/FrY2",
+               "https://esm.sh/graphiql@5.2.2": "sha384-MBVZMq1pmz8DwpwIWPWLk2tmS6tGiSi6WwbXvy9NhuDYASAAWd2m96xbxLqszig9",
+               "https://esm.sh/graphiql@5.2.2?standalone&external=react,react-dom,@graphiql/react,graphql": "sha384-SzHBEbcQfhvmwqh5Vtat9k7b/kIzmdVO3KMzQiAYwcxCA9x7vZwFRUgjzN1AeV3q",
+               "https://esm.sh/@graphiql/plugin-explorer@5.1.1": "sha384-83REbLb9KtIhL/6J1n91SLoP0648KOKZLIDdHRx/a0E7T3ajq6PzKz+815SCfN52",
+               "https://esm.sh/@graphiql/react@0.37.3?standalone&external=react,react-dom,graphql,@graphiql/toolkit,@emotion/is-prop-valid": "sha384-iZsbTy9B0VcX2BOTdqMuX0uJ9Hff5GbG2QeOt4OeMp0GHza76dwQaYQYNYkZkIVq",
+               "https://esm.sh/@graphiql/toolkit@0.11.3?standalone&external=graphql": "sha384-ZsnupyYmzpNjF1Z/81zwi4nV352n4P7vm0JOFKiYnAwVGOf9twnEMnnxmxabMBXe",
+               "https://esm.sh/graphql@16.13.2": "sha384-TQg9alwG3P9fzBErDW011vKuyTnrwpBZsl3SdMAh6DwBcv9ezFOl0djGI/68VOyy"
+             }
+           }
+         </script>
+         <script type="module">
+           import React from 'react';
+           import ReactDOM from 'react-dom/client';
+           import { GraphiQL, HISTORY_PLUGIN } from 'graphiql';
+           import { createGraphiQLFetcher } from '@graphiql/toolkit';
+           import { explorerPlugin } from '@graphiql/plugin-explorer';
+           import 'graphiql/setup-workers/esm.sh';
 
-         
-           var fetcher = graphQLHttpFetcher;
-         
+           const fetcher = createGraphiQLFetcher({
+             url: "/v1/graphql",
+           });
+           const plugins = [HISTORY_PLUGIN, explorerPlugin()];
 
-         // When the query and variables string is edited, update the URL bar so
-         // that it can be easily shared.
-         function onEditQuery(newQuery) {
-           parameters.query = newQuery;
-           
-         }
-         function onEditVariables(newVariables) {
-           parameters.variables = newVariables;
-           
-         }
-         function onEditOperationName(newOperationName) {
-           parameters.operationName = newOperationName;
-           
-         }
-         function updateURL() {
-           var cleanParams = Object.keys(parameters).filter(function(v) {
-             return parameters[v];
-           }).reduce(function(old, v) {
-             old[v] = parameters[v];
-             return old;
-           }, {});
+           function App() {
+             return React.createElement(GraphiQL, {
+               fetcher,
+               plugins,
+               defaultEditorToolsVisibility: true,
+               initialQuery: \`\`,
+               initialHeaders: \`{"Authorization":"Bearer oudelali"}\`,
+             });
+           }
 
-           history.replaceState(null, null, locationQuery(cleanParams) + window.location.hash);
-         }
-         // Render <GraphiQL /> into the body.
-         ReactDOM.render(
-           React.createElement(GraphiQL, {
-             fetcher: fetcher,
-             onEditQuery: onEditQuery,
-             onEditVariables: onEditVariables,
-             onEditOperationName: onEditOperationName,
-             query: null,
-             response: null,
-             variables: null,
-             operationName: null,
-             editorTheme: null,
-             websocketConnectionParams: null,
-           }),
-           document.body
-         );
-       </script>
-     </body>
+           const container = document.getElementById('graphiql');
+           const root = ReactDOM.createRoot(container);
+           root.render(React.createElement(App));
+         </script>
+       </head>
+       <body>
+         <div id="graphiql">
+           <div class="loading">Loading…</div>
+         </div>
+       </body>
      </html>",
        "debugCalls": [
          [
--- packages/whook-graphiql/src/index.ts
+++ packages/whook-graphiql/src/index.ts
@@ -1,6 +1,4 @@
-import url from 'url';
 import { type IncomingMessage, type ServerResponse } from 'node:http';
-import * as GraphiQL from 'apollo-server-module-graphiql';
 import { wrapInitializer, alsoInject } from 'knifecycle';
 import { printStackTrace } from 'yerror';
 import {
@@ -99,20 +97,13 @@ export default function wrapHTTPRouterWithGraphIQL<D extends Dependencies>(
         res: ServerResponse,
       ) {
         if (req.url?.startsWith(GRAPHIQL.path)) {
-          const query = (req.url && url.parse(req.url, true).query) || {};
-          return GraphiQL.resolveGraphiQLString(
-            query,
-            {
-              endpointURL: `${BASE_PATH}${GRAPHIQL.graphQLPath || '/graphql'}`,
-              query: GRAPHIQL.defaultQuery,
-              ...(DEV_ACCESS_TOKEN
-                ? {
-                    passHeader: `'Authorization': '${DEV_ACCESS_MECHANISM} ${DEV_ACCESS_TOKEN}'`,
-                  }
-                : {}),
-            },
-            req,
-          ).then(
+          return resolveGraphiQLString({
+            endpointURL: `${BASE_PATH}${GRAPHIQL.graphQLPath || '/graphql'}`,
+            query: GRAPHIQL.defaultQuery,
+            headers: DEV_ACCESS_TOKEN
+              ? { Authorization: `${DEV_ACCESS_MECHANISM} ${DEV_ACCESS_TOKEN}` }
+              : {},
+          }).then(
             (graphiqlString) => {
               res.setHeader('Content-Type', 'text/html');
               res.write(graphiqlString);
@@ -130,4 +121,117 @@ export default function wrapHTTPRouterWithGraphIQL<D extends Dependencies>(
     },
     augmentedInitializer,
   );
+}
+
+// See https://github.com/graphql/graphiql/blob/graphiql%405.2.3/examples/graphiql-cdn/index.html
+export async function resolveGraphiQLString({
+  endpointURL,
+  query,
+  headers = {},
+}: {
+  endpointURL: string;
+  query: string;
+  headers?: Record<string, string>;
+}) {
+  return `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Whook GraphiQL Explorer</title>
+    <style>
+      body {
+        margin: 0;
+      }
+
+      #graphiql {
+        height: 100dvh;
+      }
+
+      .loading {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 4rem;
+      }
+    </style>
+    <link
+      rel="stylesheet"
+      href="https://esm.sh/graphiql@5.2.2/dist/style.css"
+      integrity="sha384-f6GHLfCwoa4MFYUMd3rieGOsIVAte/evKbJhMigNdzUf52U9bV2JQBMQLke0ua+2"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://esm.sh/@graphiql/plugin-explorer@5.1.1/dist/style.css"
+      integrity="sha384-vTFGj0krVqwFXLB7kq/VHR0/j2+cCT/B63rge2mULaqnib2OX7DVLUVksTlqvMab"
+      crossorigin="anonymous"
+    />
+    <!--
+     * Note:
+     * The ?standalone flag bundles the module along with all of its \`dependencies\`, excluding \`peerDependencies\`, into a single JavaScript file.
+     * \`@emotion/is-prop-valid\` is a shim to remove the console error \` module "@emotion /is-prop-valid" not found\`. Upstream issue: https://github.com/motiondivision/motion/issues/3126
+    -->
+    <script type="importmap">
+      {
+        "imports": {
+          "react": "https://esm.sh/react@19.2.5",
+          "react/": "https://esm.sh/react@19.2.5/",
+          "react-dom": "https://esm.sh/react-dom@19.2.5",
+          "react-dom/": "https://esm.sh/react-dom@19.2.5/",
+          "graphiql": "https://esm.sh/graphiql@5.2.2?standalone&external=react,react-dom,@graphiql/react,graphql",
+          "graphiql/": "https://esm.sh/graphiql@5.2.2/",
+          "@graphiql/plugin-explorer": "https://esm.sh/@graphiql/plugin-explorer@5.1.1?standalone&external=react,@graphiql/react,graphql",
+          "@graphiql/react": "https://esm.sh/@graphiql/react@0.37.3?standalone&external=react,react-dom,graphql,@graphiql/toolkit,@emotion/is-prop-valid",
+          "@graphiql/toolkit": "https://esm.sh/@graphiql/toolkit@0.11.3?standalone&external=graphql",
+          "graphql": "https://esm.sh/graphql@16.13.2",
+          "@emotion/is-prop-valid": "data:text/javascript,"
+        },
+        "integrity": {
+          "https://esm.sh/react@19.2.5": "sha384-ZNmUQ9QQgyl95nnD/FJTBQn2ZEPTbWtMuWCXTKWNuF6Si7nC+6bvSgk5LWu+ELHn",
+          "https://esm.sh/react-dom@19.2.5": "sha384-qtNxBzn9gBs3CmJItMuvIVyjW3VIU0/rzGhCm9MippVU1BpR/c4VgaFYDIg/FrY2",
+          "https://esm.sh/graphiql@5.2.2": "sha384-MBVZMq1pmz8DwpwIWPWLk2tmS6tGiSi6WwbXvy9NhuDYASAAWd2m96xbxLqszig9",
+          "https://esm.sh/graphiql@5.2.2?standalone&external=react,react-dom,@graphiql/react,graphql": "sha384-SzHBEbcQfhvmwqh5Vtat9k7b/kIzmdVO3KMzQiAYwcxCA9x7vZwFRUgjzN1AeV3q",
+          "https://esm.sh/@graphiql/plugin-explorer@5.1.1": "sha384-83REbLb9KtIhL/6J1n91SLoP0648KOKZLIDdHRx/a0E7T3ajq6PzKz+815SCfN52",
+          "https://esm.sh/@graphiql/react@0.37.3?standalone&external=react,react-dom,graphql,@graphiql/toolkit,@emotion/is-prop-valid": "sha384-iZsbTy9B0VcX2BOTdqMuX0uJ9Hff5GbG2QeOt4OeMp0GHza76dwQaYQYNYkZkIVq",
+          "https://esm.sh/@graphiql/toolkit@0.11.3?standalone&external=graphql": "sha384-ZsnupyYmzpNjF1Z/81zwi4nV352n4P7vm0JOFKiYnAwVGOf9twnEMnnxmxabMBXe",
+          "https://esm.sh/graphql@16.13.2": "sha384-TQg9alwG3P9fzBErDW011vKuyTnrwpBZsl3SdMAh6DwBcv9ezFOl0djGI/68VOyy"
+        }
+      }
+    </script>
+    <script type="module">
+      import React from 'react';
+      import ReactDOM from 'react-dom/client';
+      import { GraphiQL, HISTORY_PLUGIN } from 'graphiql';
+      import { createGraphiQLFetcher } from '@graphiql/toolkit';
+      import { explorerPlugin } from '@graphiql/plugin-explorer';
+      import 'graphiql/setup-workers/esm.sh';
+
+      const fetcher = createGraphiQLFetcher({
+        url: ${JSON.stringify(endpointURL)},
+      });
+      const plugins = [HISTORY_PLUGIN, explorerPlugin()];
+
+      function App() {
+        return React.createElement(GraphiQL, {
+          fetcher,
+          plugins,
+          defaultEditorToolsVisibility: true,
+          initialQuery: \`${query.replaceAll('`', '\\`')}\`,
+          initialHeaders: \`${JSON.stringify(headers).replaceAll('\\', '\\\\').replaceAll('`', '\\`')}\`,
+        });
+      }
+
+      const container = document.getElementById('graphiql');
+      const root = ReactDOM.createRoot(container);
+      root.render(React.createElement(App));
+    </script>
+  </head>
+  <body>
+    <div id="graphiql">
+      <div class="loading">Loading…</div>
+    </div>
+  </body>
+</html>`;
 }
